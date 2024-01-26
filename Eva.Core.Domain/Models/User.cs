e2734ba2551432529cc1d4ed90964b713ed35e52
Amir Monfared <amonfared73@gmail.com>
2024-01-26 13:15:48 +0330

    Add IsAdmin property to User entity

--- Eva.Core.Domain/Models/User.cs
+++ Eva.Core.Domain/Models/User.cs
@@ -6,6 +6,7 @@ namespace Eva.Core.Domain.Models
     {
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; }
         public override string ToString() => Username;
     }
 }
---
+++ Eva.Infra.EntityFramework/Migrations/20240126094255_adminUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eva.Infra.EntityFramework.Migrations
+{
+    public partial class adminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+        }
+    }
+}
