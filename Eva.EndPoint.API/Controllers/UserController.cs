6894fc1607cf4bdc350362edc53feb1c020787d6
Amir Monfared <amonfared73@gmail.com>
2024-01-26 18:07:53 +0330

    Catch respective exception

--- Eva.EndPoint.API/Controllers/UserController.cs
+++ Eva.EndPoint.API/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using Eva.Core.ApplicationService.Services;
 using Eva.Core.Domain.BaseViewModels;
+using Eva.Core.Domain.Exceptions;
 using Eva.Core.Domain.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Eva.Core.Domain.Responses;
 
 namespace Eva.EndPoint.API.Controllers
 {
@@ -17,7 +19,24 @@ namespace Eva.EndPoint.API.Controllers
         [HttpPut]
         public async Task<ActionResultViewModel<User>> AlterAdminStateAsync(int userId)
         {
-            return await _service.AlterAdminStateAsync(userId);
+            try
+            {
+                return await _service.AlterAdminStateAsync(userId);
+            }
+            catch(EvaNotFoundException ex)
+            {
+                return new ActionResultViewModel<User>()
+                {
+                    ResponseMessage = new ResponseMessage(ex.Message),
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ActionResultViewModel<User>()
+                {
+                    ResponseMessage = new ResponseMessage(ex.Message),
+                };
+            }
         }
 
         // Users must be registered through authentication controller
