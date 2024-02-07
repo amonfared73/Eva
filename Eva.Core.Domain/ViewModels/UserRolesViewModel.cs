a08b5031055d4732153d6f9aa0a5ab8bfd85f312
Amir Monfared <amonfared73@gmail.com>
2024-02-07 22:18:41 +0330

    Introduce new user role report

---
+++ Eva.Core.Domain/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eva.Core.Domain.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
