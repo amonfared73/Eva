58be6113f4c203dc54b2b0b95a9ba26b19d2db34
Amir Monfared <amonfared73@gmail.com>
2024-02-07 22:05:16 +0330

    Implement logging services

---
+++ Eva.Core.ApplicationService/Queries/EvaLogService.cs
@@ -0,0 +1,18 @@
+﻿using Eva.Core.ApplicationService.Services;
+using Eva.Core.Domain.Attributes;
+using Eva.Core.Domain.Models;
+using Eva.Infra.EntityFramework.DbContextes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eva.Core.ApplicationService.Queries
+{
+    [DisableBaseOperations]
+    public class EvaLogService : BaseService<EvaLog>, IEvaLogService
+    {
+        private readonly IDbContextFactory<EvaDbContext> _contextFactory;
+        public EvaLogService(IDbContextFactory<EvaDbContext> contextFactory) : base(contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+    }
+}
---
+++ Eva.EndPoint.API/Controllers/EvaLogController.cs
@@ -0,0 +1,14 @@
+﻿using Eva.Core.ApplicationService.Services;
+using Eva.Core.Domain.Models;
+
+namespace Eva.EndPoint.API.Controllers
+{
+    public class EvaLogController : EvaControllerBase<EvaLog>
+    {
+        private readonly IEvaLogService _service;
+        public EvaLogController(IEvaLogService service) : base(service)
+        {
+            _service = service;
+        }
+    }
+}
