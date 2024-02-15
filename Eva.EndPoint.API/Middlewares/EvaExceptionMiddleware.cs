7a33a955132d00e2914cf3a10d32d66a4744e1f8
Amir Monfared <amonfared73@gmail.com>
2024-02-15 13:25:38 +0330

    Introduce EvaExceptionMiddleware

---
+++ Eva.EndPoint.API/Middlewares/EvaExceptionMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace Eva.EndPoint.API.Middlewares
+{
+    public class EvaExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public EvaExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
