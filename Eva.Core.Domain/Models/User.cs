79d55dd588931d328c26351a4dd544e3a1b1c8ba
Amir Monfared <amonfared73@gmail.com>
2024-02-15 09:49:41 +0330

    apply jasonIgnore attribute for use passwordhash

--- Eva.Core.Domain/Models/User.cs
+++ Eva.Core.Domain/Models/User.cs
@@ -6,6 +6,7 @@ namespace Eva.Core.Domain.Models
     public class User : DomainObject
     {
         public string Username { get; set; } = string.Empty;
+        [JsonIgnore]
         public string PasswordHash { get; set; } = string.Empty;
         public bool IsAdmin { get; set; }
         [JsonIgnore]
