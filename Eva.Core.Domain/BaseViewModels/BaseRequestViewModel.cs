4ee12ac9a47a30e11487b2c3fb11fce6fd752436
Amir Monfared <amonfared73@gmail.com>
2024-02-23 22:39:11 +0330

    Implement employee controller testing

--- Eva.Core.Domain/BaseViewModels/BaseRequestViewModel.cs
+++ Eva.Core.Domain/BaseViewModels/BaseRequestViewModel.cs
@@ -5,5 +5,28 @@
         public PaginationRequestViewModel? PaginationRequest { get; set; }
         public SortingRequestViewModel? SortingRequest { get; set; }
         public SearchTermViewModel? SearchTermRequest { get; set; }
+        public static BaseRequestViewModel SampleBaseRequestViewModel()
+        {
+            return new BaseRequestViewModel()
+            {
+                PaginationRequest = new PaginationRequestViewModel()
+                {
+                    PageNumber = 1,
+                    RecordsPerPage = 5
+                },
+                SortingRequest = new SortingRequestViewModel()
+                {
+                    SortingItem = new SortingItemViewModel()
+                    {
+                        Field = "Id",
+                        SortingType = Enums.SortingType.Ascending
+                    }
+                },
+                SearchTermRequest = new SearchTermViewModel()
+                {
+                    SearchTerm = ""
+                }
+            };
+        }
     }
 }
---
+++ Eva.UnitTest.xUnit/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Eva.Core.ApplicationService.Services;
+using Eva.Core.Domain.BaseViewModels;
+using Eva.EndPoint.API.Controllers;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eva.UnitTest.xUnit.Controllers
+{
+    public class EmployeeControllerTest
+    {
+        [Fact]
+        public void EmployeeGetAll()
+        {
+            // Arrange
+            var employeeService = A.Fake<IEmployeeService>();
+            var employeeController = new EmployeeController(employeeService);
+            var getAllInput = BaseRequestViewModel.SampleBaseRequestViewModel();
+
+            // Act
+            var actionResult = employeeController.GetAllAsync(getAllInput);
+            var okResult = new OkObjectResult(actionResult);
+
+            // Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+    }
+}
