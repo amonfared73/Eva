6fb94c105f0357ea7310c90c4f1661d917e6c1ea
Amir Monfared <amonfared73@gmail.com>
2024-02-23 10:43:18 +0330

    override tostring method for EvaLogReportOutputViewModel

--- Eva.Core.Domain/ViewModels/EvaLogReportOutputViewModel.cs
+++ Eva.Core.Domain/ViewModels/EvaLogReportOutputViewModel.cs
@@ -10,5 +10,9 @@ namespace Eva.Core.Domain.ViewModels
         public string StatusCode { get; set; } = string.Empty;
         public string? Payload { get; set; }
         public string? Response { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2}, {3}, {4}, {5}", Username, RequestUrl, RequestMethod, StatusCode, Payload, Response);
+        }
     }
 }
