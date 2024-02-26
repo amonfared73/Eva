e496117189171c833368065399d00755bf404a36
Amir Monfared <amonfared73@gmail.com>
2024-02-26 08:55:06 +0330

    remove unwanted usings

--- Eva.Core.Domain/BaseModels/ActiveRoles.cs
+++ Eva.Core.Domain/BaseModels/ActiveRoles.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Eva.Core.Domain.BaseModels
+﻿namespace Eva.Core.Domain.BaseModels
 {
     public static class ActiveRoles
     {
