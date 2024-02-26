754cd7a1e00705ebc4e3cd1422eb08cf93a5f3ec
Amir Monfared <amonfared73@gmail.com>
2024-02-27 03:19:37 +0330

    Implement account controller

---
+++ Eva.Core.ApplicationService/Queries/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using Eva.Core.ApplicationService.Services;
+using Eva.Core.Domain.Attributes;
+using Eva.Core.Domain.Models;
+using Eva.Infra.EntityFramework.DbContextes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eva.Core.ApplicationService.Queries
+{
+    [RegistrationRequired]
+    public class AccountService : BaseService<Account>, IAccountService
+    {
+        private readonly IDbContextFactory<EvaDbContext> _dbContextFactory;
+        public AccountService(IDbContextFactory<EvaDbContext> dbContextFactory) : base(dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+    }
+}
---
+++ Eva.EndPoint.API/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Eva.Core.ApplicationService.Services;
+using Eva.Core.Domain.Models;
+
+namespace Eva.EndPoint.API.Controllers
+{
+    public class AccountController : EvaControllerBase<Account>
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService) : base(accountService)
+        {
+            _accountService = accountService;
+        }
+    }
+}
