63fa01dcd3af9920d8b32d36be16176bbfaa272f
Amir Monfared <amonfared73@gmail.com>
2024-02-26 13:20:34 +0330

    Introduce EvaProject as an Enumeration Class

---
+++ Eva.Core.Domain/BaseModels/EvaProject.cs
@@ -0,0 +1,20 @@
+﻿namespace Eva.Core.Domain.BaseModels
+{
+    public class EvaProject : Enumeration
+    {
+        public static readonly EvaProject Domain = new EvaProject(1, "Eva.Core.Domain");
+        public static readonly EvaProject ApplicationService = new EvaProject(2, "Eva.Core.ApplicationService");
+        public static readonly EvaProject EntityFramework = new EvaProject(3, "Eva.Infra.EntityFramework");
+        public static readonly EvaProject Tools = new EvaProject(4, "Eva.Infra.Tools");
+        public static readonly EvaProject API = new EvaProject(5, "Eva.EndPoint.API");
+        public static readonly EvaProject xUnit = new EvaProject(6, "Eva.UnitTest.xUnit");
+        private EvaProject()
+        {
+
+        }
+        private EvaProject(int id, string value) : base(id, value)
+        {
+
+        }
+    }
+}
