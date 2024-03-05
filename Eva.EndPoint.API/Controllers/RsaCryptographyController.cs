292a4141ec21f0477f185ca191d73576f69f1bc1
Amir Monfared <amonfared73@gmail.com>
2024-03-05 08:48:07 +0330

    Add meaningfull comments to RSA Controller

--- Eva.EndPoint.API/Controllers/RsaCryptographyController.cs
+++ Eva.EndPoint.API/Controllers/RsaCryptographyController.cs
@@ -6,9 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace Eva.EndPoint.API.Controllers
 {
     [DisableBaseOperations]
+    
     public class RsaCryptographyController : EvaControllerBase<RsaCryptography>
     {
         private readonly IRsaCryptographyService _service;
+        /// <summary>
+        /// This controller provides encryption and decryption of texts by means of RSA Cryptography
+        /// This encryption currently works in one session and an encrypted text will not be decrypted in an other session
+        /// I'm working on it :)
+        /// </summary>
+        /// <param name="service"></param>
         public RsaCryptographyController(IRsaCryptographyService service) : base(service)
         {
             _service = service;
