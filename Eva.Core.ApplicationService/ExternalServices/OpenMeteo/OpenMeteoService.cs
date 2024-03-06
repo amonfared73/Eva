4f0379997dabe35bdeb42fe53472f14191c2e7ec
Amir Monfared <amonfared73@gmail.com>
2024-03-06 13:46:37 +0330

    fix Open Meteo weatherForcast service

--- Eva.Core.ApplicationService/ExternalServices/OpenMeteo/OpenMeteoService.cs
+++ Eva.Core.ApplicationService/ExternalServices/OpenMeteo/OpenMeteoService.cs
@@ -16,7 +16,7 @@ namespace Eva.Core.ApplicationService.ExternalServices.OpenMeteo
         }
         public async Task<WeatherForcastResultViewModel> ForcastAsync(WeatherForcastViewModel reqeust)
         {
-            var url = string.Format("{0}?latitude={1}&longitude={2}&hourly=temperature_2m", WeatherForcast.WeatherForcastUrl, reqeust.Latitude.ToString(), reqeust.Longitude.ToString());
+            var url = $"{WeatherForcast.WeatherForcastUrl}?latitude={reqeust.Latitude.ToString()}&longitude={reqeust.Longitude.ToString()}&hourly=temperature_2m";
             var response = await _httpClient.GetAsync(url);
 
             try
