73803c65d04d765f09dde499cb12b65c26315262
Amir Monfared <amonfared73@gmail.com>
2024-03-08 11:29:17 +0330

    introduce ignore update attributte

--- Eva.Core.Domain/BaseModels/Trackable.cs
+++ Eva.Core.Domain/BaseModels/Trackable.cs
@@ -1,8 +1,12 @@
-﻿namespace Eva.Core.Domain.BaseModels
+﻿using Eva.Core.Domain.Attributes;
+
+namespace Eva.Core.Domain.BaseModels
 {
     public abstract class Trackable
     {
+        [IgnoreUpdate]
         public int CreatedBy { get; set; }
+        [IgnoreUpdate]
         public DateTime CreatedOn { get; set; }
         public int ModifiedBy { get; set; }
         public DateTime ModifiedOn { get; set; }
