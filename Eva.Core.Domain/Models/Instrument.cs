785c9e863d42d5e73e643dd7c627b8127511cadc
Amir Monfared <amonfared73@gmail.com>
2024-03-10 14:05:21 +0330

    update migrations

--- Eva.Core.Domain/Models/Instrument.cs
+++ Eva.Core.Domain/Models/Instrument.cs
@@ -4,6 +4,7 @@ namespace Eva.Core.Domain.Models
 {
     public class Instrument : DomainObject
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int YearInvented { get; set; }
     }
 }
