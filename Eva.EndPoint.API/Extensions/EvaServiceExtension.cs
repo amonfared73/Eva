cdb61bd220d537557992aa5a33bcd7825d264477
Amir Monfared <amonfared73@gmail.com>
2024-03-13 09:48:18 +0330

    update addEva comments

--- Eva.EndPoint.API/Extensions/EvaServiceExtension.cs
+++ Eva.EndPoint.API/Extensions/EvaServiceExtension.cs
@@ -26,7 +26,7 @@ namespace Eva.EndPoint.API.Extensions
     {
         private static IServiceCollection AddEvaDbContext(this IServiceCollection services, string connectionString)
         {
-            services.AddSingleton<IEvaDbContextFactory,EvaDbContextFactory>();
+            services.AddSingleton<IEvaDbContextFactory, EvaDbContextFactory>();
             services.AddDbContextFactory<EvaDbContext>(options => options.UseSqlite(connectionString));
             return services;
         }
@@ -81,12 +81,21 @@ namespace Eva.EndPoint.API.Extensions
         }
 
         /// <summary>
-        ///     <para>
-        ///         Initializes Eva Framework Asp.Net Core web api application
-        ///     </para>
-        /// <param name="app">an Eva WebApplication<see cref="WebApplication" /> to observe while waiting for the task to complete.</param>
+        /// Initializes <see href="https://github.com/amonfared73/Eva">Eva</see> Framework asp.Net Core web api application
+        /// <para>
+        /// Grabing sensitive configuration data from <see cref="IConfiguration" />
+        /// <para>Creating instances of related objects and injecting them into HTTP pipeline</para>
+        /// <para>Configuring controllers with their custom respective <see cref="IControllerModelConvention" /></para>
+        /// <para>Configuring authentication and authorization</para>
+        /// <para>Registering <see href="https://github.com/amonfared73/Eva">Eva</see> services</para>
+        /// <para>Adding related middlewares to HTTP pipeline</para>
+        /// </para>
+        /// <para>
+        /// <param name="app">an <see href="https://github.com/amonfared73/Eva">Eva</see> <see cref="WebApplication" /> out parameter used to configure the HTTP pipeline and routes</param>
+        /// </para>
+        /// </summary>
         /// <returns>
-        ///     A WebApplicationBuilder that represents the Eva Framework builder
+        /// A <see cref="WebApplicationBuilder" /> that represents the <see href="https://github.com/amonfared73/Eva">Eva</see> Framework builder
         /// </returns>
         public static WebApplicationBuilder AddEva(this WebApplicationBuilder builder, out WebApplication app)
         {
