f2d12e89a746574bccb9576aa6648bcb382189f4
Amir Monfared <amonfared73@gmail.com>
2024-03-16 10:27:52 +0330

    Implement IUserContext

--- Eva.EndPoint.API/Extensions/EvaServiceExtension.cs
+++ Eva.EndPoint.API/Extensions/EvaServiceExtension.cs
@@ -19,7 +19,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
-using System.Text.Json.Serialization;
 
 namespace Eva.EndPoint.API.Extensions
 {
