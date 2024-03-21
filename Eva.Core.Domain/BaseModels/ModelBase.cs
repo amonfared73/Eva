c689610445092088b072da2f15b831b69244e37d
Amir Monfared <amonfared73@gmail.com>
2024-03-21 08:27:00 +0430

    introduce companyId and IsDeleted properties

--- Eva.Core.Domain/BaseModels/ModelBase.cs
+++ Eva.Core.Domain/BaseModels/ModelBase.cs
@@ -1,9 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Eva.Core.Domain.BaseModels
+﻿namespace Eva.Core.Domain.BaseModels
 {
+    /// <summary>
+    /// The base entity model for all Eva models
+    /// </summary>
     public class ModelBase : Trackable
     {
+        /// <summary>
+        /// The unique integer key for all Eva entities
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Entity sate code for entity instances, default = 1
+        /// </summary>
+        public int StateCode { get; set; } = 1;
+        /// <summary>
+        /// IsDeleted property for soft delete implementation
+        /// </summary>
+        public bool IsDeleted { get; set; } = false;
     }
 }
