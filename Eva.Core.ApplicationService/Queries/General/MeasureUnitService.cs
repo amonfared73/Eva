65ed32b12f3235806c632fdc5e6907353210cc1d
Amir Monfared <amonfared73@gmail.com>
2024-03-26 10:09:52 +0330

    Implement services

---
+++ Eva.Core.ApplicationService/Queries/General/MeasureUnitService.cs
@@ -0,0 +1,17 @@
+﻿using Eva.Core.Domain.Attributes;
+using Eva.Core.Domain.Models.General;
+using Eva.Core.Domain.ViewModels.General;
+using Eva.Infra.EntityFramework.DbContextes;
+
+namespace Eva.Core.ApplicationService.Queries.General
+{
+    [RegistrationRequired]
+    public class MeasureUnitService : BaseService<MeasureUnit, MeasureUnitViewModel>
+    {
+        private readonly IEvaDbContextFactory _dbContextFactory;
+        public MeasureUnitService(IEvaDbContextFactory dbContextFactory) : base(dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+    }
+}
---
+++ Eva.Core.ApplicationService/Queries/Inv/TransactionService.cs
@@ -0,0 +1,17 @@
+﻿using Eva.Core.Domain.Attributes;
+using Eva.Core.Domain.Models.Inv;
+using Eva.Core.Domain.ViewModels.Inv;
+using Eva.Infra.EntityFramework.DbContextes;
+
+namespace Eva.Core.ApplicationService.Queries.Inv
+{
+    [RegistrationRequired]
+    public class TransactionService : BaseService<Transaction, TransactionViewModel>
+    {
+        private readonly IEvaDbContextFactory _dbContextFactory;
+        public TransactionService(IEvaDbContextFactory dbContextFactory) : base(dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+    }
+}
