6a1714881bfc179f50730f61906d3dfe170b4f61
Amir Monfared <amonfared73@gmail.com>
2024-03-26 10:24:18 +0330

    Implement controllers

---
+++ Eva.EndPoint.API/Controllers/General/GoodController.cs
@@ -0,0 +1,15 @@
+﻿using Eva.Core.ApplicationService.Services.General;
+using Eva.Core.Domain.Models.General;
+using Eva.Core.Domain.ViewModels.General;
+
+namespace Eva.EndPoint.API.Controllers.General
+{
+    public class GoodController : EvaControllerBase<Good, GoodViewModel>
+    {
+        private readonly IGoodService _goodService;
+        public GoodController(IGoodService goodService) : base(goodService)
+        {
+            _goodService = goodService;
+        }
+    }
+}
