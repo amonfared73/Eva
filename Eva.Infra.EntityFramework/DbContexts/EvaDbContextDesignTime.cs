a393e1e10181c97bab017053f9a36dbe5257b546
Amir Monfared <amonfared73@gmail.com>
2024-03-30 08:11:28 +0330

    mroe renaming

--- Eva.Infra.EntityFramework/DbContexts/EvaDbContextDesignTime.cs
+++ Eva.Infra.EntityFramework/DbContexts/EvaDbContextDesignTime.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace Eva.Infra.EntityFramework.DbContexts
