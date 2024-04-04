6b7e40563651ad69f2c95475f57465b0b30b7a4d
Amir Monfared <amonfared73@gmail.com>
2024-04-04 21:15:43 +0430

    finally done!

--- Eva.EndPoint.API/Extensions/IConfigurationExtensions.cs
+++ Eva.EndPoint.API/Extensions/IConfigurationExtensions.cs
@@ -1,18 +1,27 @@
 ﻿using Eva.Core.Domain.BaseInterfaces;
 using Eva.Core.Domain.BaseModels;
-using Microsoft.Extensions.Configuration;
 
 namespace Eva.EndPoint.API.Extensions
 {
     public static class IConfigurationExtensions
     {
         private static string CurrentDb = "sqlite";
+        /// <summary>
+        /// Get <see href="https://github.com/amonfared73/Eva">Eva</see> Authentication Configuration
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static AuthenticationConfiguration GetEvaAuthenticationConfiguration(this IConfiguration configuration)
         {
             var authenticationConfiguration = new AuthenticationConfiguration();
             configuration.Bind("AuthenticationConfiguration", authenticationConfiguration);
             return authenticationConfiguration;
         }
+        /// <summary>
+        /// Get <see href="https://github.com/amonfared73/Eva">Eva</see> Connection string
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns><see cref="AuthenticationConfiguration"/></returns>
         public static string GetEvaConnectionString(this IConfiguration configuration)
         {
             return configuration.GetConnectionString(CurrentDb);
@@ -29,6 +38,11 @@ namespace Eva.EndPoint.API.Extensions
             }
             return instances;
         }
+        /// <summary>
+        /// Accessing an <see cref="IConfiguration"/> instance to reach <see href="https://github.com/amonfared73/Eva">Eva</see> appsettings.json
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns><see cref="IConfiguration"/></returns>
         public static IConfiguration GetEvaConfigurations(this WebApplicationBuilder builder)
         {
             return builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
