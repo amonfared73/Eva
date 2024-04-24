47e1e96ddd3c5be24e357b42db5d9b49ea78ab32
Amir Monfared <amonfared73@gmail.com>
2024-04-24 15:54:13 +0330

    Introduce account model

--- Eva.Core.Domain/Models/Account.cs
+++ Eva.Core.Domain/Models/Account.cs
@@ -7,6 +7,8 @@ namespace Eva.Core.Domain.Models
     public class Account : ModelBase
     {
         public string Name { get; set; } = string.Empty;
+        public int ParentId { get; set; }
+        public Account Parent { get; set; }
         public IEnumerable<Account> Accounts { get; set; }
     }
 }
