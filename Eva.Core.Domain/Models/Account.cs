93a4aaaa0954625b130ea6b910e9f243ffb3507e
Amir Monfared <amonfared73@gmail.com>
2024-04-27 09:07:55 +0330

    add comment

--- Eva.Core.Domain/Models/Account.cs
+++ Eva.Core.Domain/Models/Account.cs
@@ -11,6 +11,11 @@ namespace Eva.Core.Domain.Models
         public int ParentId { get; set; }
         public Account Parent { get; set; }
         public IEnumerable<Account> Accounts { get; set; }
+        /// <summary>
+        /// Implicit conversion for converting a <see cref="RootAccountDto"/> to an Account model
+        /// This conversion is used to create a top level account with no parent
+        /// </summary>
+        /// <param name="accountDto"></param>
         public static implicit operator Account(RootAccountDto accountDto)
         {
             return new Account()
