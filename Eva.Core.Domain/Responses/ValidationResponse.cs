9c1b7129075242e7369056b55114363b45bcb081
Amir Monfared <amonfared73@gmail.com>
2024-05-06 08:59:53 +0330

    update comments

--- Eva.Core.Domain/Responses/ValidationResponse.cs
+++ Eva.Core.Domain/Responses/ValidationResponse.cs
@@ -1,5 +1,8 @@
 ﻿namespace Eva.Core.Domain.Responses
 {
+    /// <summary>
+    /// The default response of validation process, consists of a boolean parameter to checks its validity and a <see cref="ResponseMessage"/> to hold respective messages
+    /// </summary>
     public class ValidationResponse
     {
         public bool IsValid { get; set; }
