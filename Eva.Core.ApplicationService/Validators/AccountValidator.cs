90bf25bca056fb253e6308e20255eefc4ece6df6
Amir Monfared <amonfared73@gmail.com>
2024-05-06 07:32:53 +0330

    introduce AccountValidator

---
+++ Eva.Core.ApplicationService/Validators/AccountValidator.cs
@@ -0,0 +1,30 @@
+﻿using Eva.Core.Domain.Models;
+using Eva.Core.Domain.Responses;
+using Eva.Core.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eva.Core.ApplicationService.Validators
+{
+    public class AccountValidator
+    {
+        private bool _isValid;
+        private List<string> _messages;
+        public AccountValidatorResponseViewModel Validate(Account user)
+        {
+            _messages.Clear();
+
+            
+
+
+            return new AccountValidatorResponseViewModel()
+            {
+                IsValid = _isValid,
+                ResponseMessage = new ResponseMessage(_messages)
+            };
+        }
+    }
+}
