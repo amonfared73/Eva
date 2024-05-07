e045259445bd2081f56738dca80301a50721b211
Amir Monfared <amonfared73@gmail.com>
2024-05-07 09:42:52 +0330

    make ExtractUserPermissions distinct

--- Eva.Core.ApplicationService/Queries/RolePermissionService.cs
+++ Eva.Core.ApplicationService/Queries/RolePermissionService.cs
@@ -48,12 +48,12 @@ namespace Eva.Core.ApplicationService.Queries
         {
             using (var context = _dbContextFactory.CreateDbContext())
             {
-                var userRoles = await context.UserRoleMappings.Where(ur => ur.UserId == userId).Select(ur => new {ur.UserId, ur.RoleId}).ToListAsync();
-                var permissions = await context.RolePermissionMappings.Include(p => p.Permission).Select(p => new {p.RoleId, p.PermissionId, p.Permission.Name}).ToListAsync();
+                var userRoles = await context.UserRoleMappings.Where(ur => ur.UserId == userId).Select(ur => new { ur.UserId, ur.RoleId }).ToListAsync();
+                var permissions = await context.RolePermissionMappings.Include(p => p.Permission).Select(p => new { p.RoleId, p.PermissionId, p.Permission.Name }).ToListAsync();
 
-                var userPermissions = from userRole in userRoles
-                                      join permission in permissions on userRole.RoleId equals permission.RoleId
-                                      select permission.Name;
+                var userPermissions = (from userRole in userRoles
+                                       join permission in permissions on userRole.RoleId equals permission.RoleId
+                                       select permission.Name).Distinct();
                 return userPermissions;
             }
         }
