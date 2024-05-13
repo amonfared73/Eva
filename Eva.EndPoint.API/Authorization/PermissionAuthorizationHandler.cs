fcb6b930ab978b5a4d26a906df75734093ffc34a
Amir Monfared <amonfared73@gmail.com>
2024-05-13 11:49:42 +0330

    Fully implemented role based authorization

--- Eva.EndPoint.API/Authorization/PermissionAuthorizationHandler.cs
+++ Eva.EndPoint.API/Authorization/PermissionAuthorizationHandler.cs
@@ -7,16 +7,18 @@ namespace Eva.EndPoint.API.Authorization
 {
     public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
     {
+        private readonly IUserContext _userContext;
         private readonly IPermissionEndPointMappingService _permissionEndPointMappingService;
         private readonly IMemoryCache _memoryCache;
         private readonly AuthenticationConfiguration _authenticationConfiguration;
         private readonly EvaCachingKeys _keys;
-        public PermissionAuthorizationHandler(IPermissionEndPointMappingService permissionEndPointMappingService, IMemoryCache memoryCache, AuthenticationConfiguration authenticationConfiguration, EvaCachingKeys keys)
+        public PermissionAuthorizationHandler(IPermissionEndPointMappingService permissionEndPointMappingService, IMemoryCache memoryCache, AuthenticationConfiguration authenticationConfiguration, EvaCachingKeys keys, IUserContext userContext)
         {
             _permissionEndPointMappingService = permissionEndPointMappingService;
             _memoryCache = memoryCache;
             _authenticationConfiguration = authenticationConfiguration;
             _keys = keys;
+            _userContext = userContext;
         }
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
         {
@@ -28,14 +30,16 @@ namespace Eva.EndPoint.API.Authorization
                 .Select(x => x.Value)
                 .ToHashSet();
 
+            string desiredCacheKey = $"{_keys.AccessibleEndPoints} {_userContext.UserId}";
+
             // Call accessible endpoints service
             HashSet<string> accessibleEndPoints;
-            accessibleEndPoints = _memoryCache.Get<HashSet<string>>(_keys.AccessibleEndPoints);
+            accessibleEndPoints = _memoryCache.Get<HashSet<string>>(desiredCacheKey);
 
             if (accessibleEndPoints is null)
             {
                 accessibleEndPoints = await _permissionEndPointMappingService.GetAccessibleEndPoints(permissions);
-                _memoryCache.Set(_keys.AccessibleEndPoints, accessibleEndPoints, TimeSpan.FromMinutes(_authenticationConfiguration.AccessTokenExpirationMinutes));
+                _memoryCache.Set(desiredCacheKey, accessibleEndPoints, TimeSpan.FromMinutes(_authenticationConfiguration.AccessTokenExpirationMinutes));
             }
 
             bool hasAccess = accessibleEndPoints.Any(uri => uri.Contains(requirement.Permission)) || permissions.Contains(requirement.Permission);
