f2c38d27cb9bc4f801236c9ee7acbf32041e6a5c
Amir Monfared <amonfared73@gmail.com>
2024-05-13 14:02:14 +0330

    Modify HasAccessAttribute

--- Eva.EndPoint.API/Authorization/HasAccessAttribute.cs
+++ Eva.EndPoint.API/Authorization/HasAccessAttribute.cs
@@ -4,12 +4,12 @@ namespace Eva.EndPoint.API.Authorization
 {
     /// <summary>
     /// This attribute will apply policy based authorization, the policy could be the name of decorating endpoint or the permission
-    /// Both controllers and Action methods can be decorated with <see cref="HasPermissionAttribute"/>
+    /// Both controllers and Action methods can be decorated with <see cref="HasAccessAttribute"/>
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
-    public sealed class HasPermissionAttribute : AuthorizeAttribute
+    public sealed class HasAccessAttribute : AuthorizeAttribute
     {
-        public HasPermissionAttribute(string policy) : base(policy)
+        public HasAccessAttribute(string policy) : base(policy)
         {
         }
     }
