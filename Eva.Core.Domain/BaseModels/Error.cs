aca8aa37e0b85ed7af60e2c0ab9c068a4612c232
Amir Monfared <amonfared73@gmail.com>
2024-06-18 09:57:34 +0330

    implement railway iriented programming

---
+++ Eva.Core.Domain/BaseModels/Error.cs
@@ -0,0 +1,11 @@
+﻿using Eva.Core.Domain.Enums;
+
+namespace Eva.Core.Domain.BaseModels
+{
+    public record Error(ErrorType type, string description)
+    {
+        public static Error InvalidComment = new Error(ErrorType.Failure, "No post found for this comment");
+        public static Error NotEnoughCharacters = new Error(ErrorType.Failure, "Comment does not have enough characters");
+        public static Error PostNotFound = new Error(ErrorType.Failure, "Post not found");
+    }
+}
