72c55185a9ff457dc5ec792658279ccb1357859d
Amir Monfared <amonfared73@gmail.com>
2024-06-18 10:08:00 +0330

    changing result names

---
+++ Eva.Infra.Tools/Extensions/EvaResultExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Eva.Core.Domain.BaseModels;
+
+namespace Eva.Infra.Tools.Extensions
+{
+    public static class EvaResultExtensions
+    {
+        public static EvaResult<TOut> Bind<TIn, TOut>(this EvaResult<TIn> result, Func<TIn, EvaResult<TOut>> bind)
+        {
+            return result.IsSuccess ?
+                bind(result.Value) :
+                EvaResult<TOut>.Failure(result.Error);
+        }
+
+        public static EvaResult<TOut> TryCatch<TIn, TOut>(this EvaResult<TIn> result, Func<TIn, TOut> func, Error error)
+        {
+            try
+            {
+                return result.IsSuccess ?
+                    EvaResult<TOut>.Success(func(result.Value)) :
+                    EvaResult<TOut>.Failure(result.Error);
+            }
+            catch
+            {
+                return EvaResult<TOut>.Failure(error);
+            }
+        }
+
+        public static EvaResult<TIn> Tap<TIn>(this EvaResult<TIn> result, Action<TIn> action)
+        {
+            if (result.IsSuccess)
+                action(result.Value);
+            return result;
+        }
+
+        public static TOut Match<TIn, TOut>(this EvaResult<TIn> result, Func<TIn, TOut> onSuccess, Func<Error, TOut> onFailure)
+        {
+            return result.IsSuccess ?
+                onSuccess(result.Value) :
+                onFailure(result.Error);
+        }
+    }
+}
